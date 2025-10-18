2d727bfa7e8499403dedde9ba7daab90f5f3d957
yaozhenxiu <yaozhenxiu@bicv.com>
2025-10-18 10:27:12 +0800

    Update OpenWrt build environment to use new container image q946666800/openwrt-build:v23.05_3, addressing libelf dependency issues and ensuring consistency in the build process.

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 # 构建新版本镜像（修复 libelf 依赖问题）
-docker build -t q946666800/openwrt-build:v23.05_2 .
-docker push q946666800/openwrt-build:v23.05_2
+docker build -t q946666800/openwrt-build:v23.05_3 .
+docker push q946666800/openwrt-build:v23.05_3
